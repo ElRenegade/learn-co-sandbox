92fd26fce9b5f1480b61065541b8cdd9012cdfb5
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-30 19:00:11 +0000

    set up directories and files

--- project_s-cli/lib/project_s/cli.rb
+++
@@ -1,8 +0,0 @@
-require "project_s/cli/version"
-
-module ProjectS
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- project_s-cli/lib/project_s/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module ProjectS
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
